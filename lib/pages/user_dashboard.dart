ca507badb9a312f134cc8710dd40051726b94c51
Samuel Adewole <samadewole381@gmail.com>
2024-11-21 22:16:17 +0100

    credit card design

--- lib/pages/user_dashboard.dart
+++ lib/pages/user_dashboard.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+import 'package:sbank/constants.dart';
 
 class UserDashboard extends StatefulWidget {
   const UserDashboard({Key? key}) : super(key: key);
@@ -12,16 +15,19 @@ class UserDashboard extends StatefulWidget {
 class _UserDashboardState extends State<UserDashboard> {
   @override
   Widget build(BuildContext context) {
+    const String fullName = "Ella star";
     return Scaffold(
         body: SingleChildScrollView(
             child: Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Ella Star",
+                Text("Hi, $fullName",
                     style:
                         TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                 CircleAvatar(
@@ -32,6 +38,7 @@ class _UserDashboardState extends State<UserDashboard> {
                 ),
               ]),
           SizedBox(height: 30),
+          Text("Total Balance", style: TextStyle(fontWeight: FontWeight.bold)),
           Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
             Text("\$ 42.00"),
             Wrap(
@@ -46,7 +53,7 @@ class _UserDashboardState extends State<UserDashboard> {
                   ),
                   child: Center(
                     child: Icon(
-                      Icons.audiotrack,
+                      Icons.add,
                       color: Colors.black,
                       size: 15.0,
                     ),
@@ -63,7 +70,7 @@ class _UserDashboardState extends State<UserDashboard> {
                   ),
                   child: Center(
                     child: Icon(
-                      Icons.audiotrack,
+                      Icons.filter_center_focus,
                       color: Colors.black,
                       size: 15.0,
                     ),
@@ -72,13 +79,76 @@ class _UserDashboardState extends State<UserDashboard> {
               ],
             )
           ]),
-          Card(
-            child: Column(children: [
+          SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+                color: primaryGreen, borderRadius: BorderRadius.circular(20)),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               Row(
-                children: [Text("money"), Text("ego")],
-              )
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 40,
+                    child: Image.asset(
+                      "images/mastercard-black.png",
+                    ),
+                  ),
+                  Transform.rotate(
+                    angle: math.pi / 6,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.rss_feed,
+                        color: Colors.black,
+                      ),
+                      onPressed: null,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Debit Card",
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 20,
+                    color: Colors.brown[50]),
+              ),
+              Text(
+                "1423 2832 8398 8432 ",
+                style: TextStyle(fontWeight: FontWeight.w800, fontSize: 24),
+              ),
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Name",
+                    style: TextStyle(color: Colors.brown[100]),
+                  ),
+                  Text(
+                    "Exp Date",
+                    style: TextStyle(color: Colors.brown[100]),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    fullName,
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                  ),
+                  Text(
+                    "06.28",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                  ),
+                ],
+              ),
             ]),
-          )
+          ),
+          SizedBox(height: 40),
         ],
       ),
     )));
